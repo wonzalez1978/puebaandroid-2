01703c50984d1713f7e43cebd9ced0c67bd3a307
Alexis Rebolledo <git@puntogestion.cl>
2020-03-10 13:30:25 -0300

    Inicio del Proyecto

---
+++ app/src/main/java/cl/puntogestion/dogapi/MainActivity.java
@@ -0,0 +1,14 @@
+package cl.puntogestion.dogapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
