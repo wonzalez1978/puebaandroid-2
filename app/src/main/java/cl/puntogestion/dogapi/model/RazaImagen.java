bd5b799faa4cfdab9bce6e7b64106ef9f91ace28
Mauricio Ponce Oyarzún <mauricio.ponce.o@gmail.com>
2020-03-13 12:10:54 -0300

    Carola Creacion raza imagen

---
+++ app/src/main/java/cl/puntogestion/dogapi/model/RazaImagen.java
@@ -0,0 +1,25 @@
+package cl.puntogestion.dogapi.model;
+
+import java.util.Map;
+
+public class RazaImagen {
+    private String status;
+    private Map<String,String>message;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public Map<String, String> getMessage() {
+        return message;
+
+    }
+
+    @Override
+    public String toString() {
+        return "RazaImagen{" +
+                "status='" + status + '\'' +
+                ", message=" + message +
+                '}';
+    }
+}
--- app/src/main/java/cl/puntogestion/dogapi/model/api/IDogDataBase.java
+++ app/src/main/java/cl/puntogestion/dogapi/model/api/IDogDataBase.java
@@ -1,11 +1,15 @@
 package cl.puntogestion.dogapi.model.api;
 
 
+import cl.puntogestion.dogapi.model.RazaImagen;
 import cl.puntogestion.dogapi.model.RazasLista;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface IDogDataBase {
     @GET("api/breeds/list/all")
     Call<RazasLista> listaRazas();
+    @GET("api/breed/{breed}/images")
+    Call<RazaImagen> listaImagenes(@Path("breed")String breed);
 }
